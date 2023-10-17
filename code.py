935078d85214f8c61a4e0c01ee5c9e7207aa28a1
Alonso J Jiménez A <alonso.jimenezanchia@ucr.ac.cr>
2023-10-17 09:25:22 -0600

    Agrego notebooks más el código para obtener los valores (x,y) de los json

--- code.py
+++ code.py
@@ -4,32 +4,31 @@ Se trata de un programa que contiene las funciones del Proyecto
 Eléctrico.
 """
 
-import json
-
-# Función para obtener centroides de archivo .json y colocarlos todos
-# en un arreglo
-
 
 def centroides_a_arreglo(json):
     """
     Función que toma archivo .json y retorna arreglo.
+
+    Función para obtener centroides de archivo .json y colocarlos todos
+    en un arreglo
 
 
     """
     # Se crea arreglo vacío para guardar los valores "x" y "y".
     xy_valores = []
 
-    # Iterate over the JSON data and extract the "x" and "y" values.
-    for frame in json_data["frame"]:
+    # Se itera sobre los datos del archivo .json y se extraen
+    # los valores "x" y "y".
+    for frame in json["frame"]:
         for circle in frame["circle"]:
-        x_value = circle["x"]
-        y_value = circle["y"]
-        xy_valores.append((x_value, y_value))
-        
+            x_value = circle["x"]
+            y_value = circle["y"]
+            xy_valores.append((x_value, y_value))
+
         return xy_valores
 
 
-
+"""
 
 from sklearn.cluster import DBSCAN
 import numpy as np
@@ -50,7 +49,8 @@ conjunto1 = [(1, 2), (3, 4), (5, 6), (10, 10)]
 conjunto2 = [(2, 3), (4, 5), (5, 6), (12, 11)]
 
 # Establece los parámetros de DBSCAN (puedes necesitar ajustarlos)
-epsilon = 1.0  # Distancia máxima entre dos muestras para ser consideradas parte del mismo grupo
+# epsilon = 1.0  # Distancia máxima entre dos muestras para ser consideradas 
+# parte del mismo grupo
 min_muestras = 2  # Número mínimo de muestras en un grupo
 
 # Agrupa las coordenadas
@@ -70,3 +70,5 @@ def comparar_etiquetas(etiquetas1, etiquetas2):
 similitud = comparar_etiquetas(etiquetas1, etiquetas2)
 
 print(f"Similitud entre los dos conjuntos de datos: {similitud}")
+
+"""
